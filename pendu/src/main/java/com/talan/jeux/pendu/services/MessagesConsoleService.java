116c549a50e3c2905c4d3649273185d518b85542
ocb90dbn <olivier-externe.cinquin@enedis.fr>
2021-05-19 14:21:17 +0200

    [func] Ajout du service d'affichage des messages dans la console, implémentation de la question du nombre de joueurs

---
+++ pendu/src/main/java/com/talan/jeux/pendu/services/MessagesConsoleService.java
@@ -0,0 +1,8 @@
+package com.talan.jeux.pendu.services;
+
+public class MessagesConsoleService {
+
+    public static void demanderLeNombreDeJoueurs() {
+        System.out.println("Nombre de joueurs (2-10) ?");
+    }
+}
