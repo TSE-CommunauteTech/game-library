19c8cf507949e64423fda244f456762d9b66d129
ocb90dbn <olivier-externe.cinquin@enedis.fr>
2023-04-27 14:06:41 +0200

    [tech] Base du jeu terminé

---
+++ pendu/src/main/java/com/talan/jeux/pendu/constants/GraphicsConstants.java
@@ -0,0 +1,77 @@
+package com.talan.jeux.pendu.constants;
+
+public class GraphicsConstants {
+
+    public static final String ERREUR0 =
+            "\t________\n" +
+            "\t|\t   \\|\n" +
+            "\t \t\t|\n" +
+            "      \t\t|\n" +
+            "\t \t\t|\n" +
+            "      \t\t|\n" +
+            "\t\t\t|\n" +
+            "   _________|";
+    public static final String ERREUR1 =
+            "\t________\n" +
+            "\t|\t   \\|\n" +
+            "\tO\t\t|\n" +
+            "      \t\t|\n" +
+            "\t \t\t|\n" +
+            "      \t\t|\n" +
+            "\t\t\t|\n" +
+            "   _________|";
+    public static final String ERREUR2 =
+            "\t________\n" +
+            "\t|\t   \\|\n" +
+            "\tO\t\t|\n" +
+            "    | \t\t|\n" +
+            "\t \t\t|\n" +
+            "      \t\t|\n" +
+            "\t\t\t|\n" +
+            "   _________|";
+    public static final String ERREUR3 =
+            "\t________\n" +
+            "\t|\t   \\|\n" +
+            "\tO\t\t|\n" +
+            "   /| \t\t|\n" +
+            "\t \t\t|\n" +
+            "      \t\t|\n" +
+            "\t\t\t|\n" +
+            "   _________|";
+    public static final String ERREUR4 =
+            "\t________\n" +
+            "\t|\t   \\|\n" +
+            "\tO\t\t|\n" +
+            "   /|\\\t\t|\n" +
+            "\t \t\t|\n" +
+            "      \t\t|\n" +
+            "\t\t\t|\n" +
+            "   _________|";
+    public static final String ERREUR5 =
+            "\t________\n" +
+            "\t|\t   \\|\n" +
+            "\tO\t\t|\n" +
+            "   /|\\\t\t|\n" +
+            "\t|\t\t|\n" +
+            "      \t\t|\n" +
+            "\t\t\t|\n" +
+            "   _________|";
+    public static final String ERREUR6 =
+            "\t________\n" +
+            "\t|\t   \\|\n" +
+            "\tO\t\t|\n" +
+            "   /|\\\t\t|\n" +
+            "\t|\t\t|\n" +
+            "   /  \t\t|\n" +
+            "\t\t\t|\n" +
+            "   _________|";
+    public static final String ERREUR7 =
+            "\t________\n" +
+            "\t|\t   \\|\n" +
+            "\tO\t\t|\n" +
+            "   /|\\\t\t|\n" +
+            "\t|\t\t|\n" +
+            "   / \\\t\t|\n" +
+            "\t\t\t|\n" +
+            "   _________|";
+}
